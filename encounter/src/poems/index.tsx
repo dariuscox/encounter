7defad31db7fa4dd2e39d56b7a975842693f749e
Darius Cox <ddcgamer243@gmail.com>
2022-01-18 21:52:16 -0800

    Feat: add capability for one poem

---
+++ encounter/src/poems/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const youlie = [
+    'YOU LIE in the great listening,',
+    'ambushed, snowed in.',
+    '',
+    'Go to the Spree, go to the Havel,',
+    'go to the butcher hooks,',
+    'to the red apple stakes',
+    'from Sweden—',
+    '',
+    'Here comes the table with the presents,',
+    'he turns around an Eden—',
+    '',
+    'The man became a sieve, the woman',
+    'had to swim, the sow,',
+    'for herself, for none, for everyone—',
+    '',
+    'The Landwehr canal will not roar.',
+    'Nothing',
+    '\tstalls',
+];
+
+export { youlie };
